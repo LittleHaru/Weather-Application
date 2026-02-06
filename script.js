39c77f4a609e7c47aef1c65949eba8eafe46ef3b
LittleHaru <107350820+LittleHaru@users.noreply.github.com>
2026-02-06 11:53:37 +0800

    refactor: prettier format writing

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 const settings = {
-    type: "metric",
-    temperature: "celsius",
-    windSpeed: "km/h",
-    percipitation: "millimeters"
+  type: "metric",
+  temperature: "celsius",
+  windSpeed: "km/h",
+  percipitation: "millimeters",
 };
 
-function assignSetting(parameter,value) {
-    settings[parameter] = value
-};
-
-function changeUnit() {
-    settings.type = (settings.type === "metric") ? "imperial" : "metric"
+function assignSetting(parameter, value) {
+  settings[parameter] = value;
 }
 
-const unitBtn = document.querySelector('.units-btn');
+function changeUnit() {
+  settings.type = settings.type === "metric" ? "imperial" : "metric";
+}
+
+const unitBtn = document.querySelector(".units-btn");
 function dropDown() {
-    document.querySelector(".dropdown-content").classList.toggle("show")
-};
-unitBtn.addEventListener('click' , dropDown)
+  document.querySelector(".dropdown-content").classList.toggle("show");
+}
+unitBtn.addEventListener("click", dropDown);
 
 const dropBtn = document.querySelectorAll(".dropdown-content button");
-dropBtn.forEach(button => {
-    const parameter = button.getAttribute('parameter')
-    const value = button.getAttribute('value')
-    if (button.getAttribute('parameter') === null || button.getAttribute('value') === null) {return}
-    button.addEventListener("click", () => {
-        assignSetting(parameter, value)
-    });
+dropBtn.forEach((button) => {
+  const parameter = button.getAttribute("parameter");
+  const value = button.getAttribute("value");
+  if (
+    button.getAttribute("parameter") === null ||
+    button.getAttribute("value") === null
+  ) {
+    return;
+  }
+  button.addEventListener("click", () => {
+    assignSetting(parameter, value);
+  });
 });
 
-const metricBtn = document.querySelector(".unit-change")
-metricBtn.addEventListener('click',changeUnit)
-
+const metricBtn = document.querySelector(".unit-change");
+metricBtn.addEventListener("click", changeUnit);
