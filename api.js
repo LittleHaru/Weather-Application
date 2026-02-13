c58333cd3a4025c4fd87c6c56bf5983b9966b978
LittleHaru <107350820+LittleHaru@users.noreply.github.com>
2026-02-13 16:55:17 +0800

    feat: add new parameters (apparent_temperature, Humidity, Wind Speed, Precipitation)

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ async function getWeatherData(latitude, longitude) {
     const params = new URLSearchParams({
         latitude,
         longitude,
-        current: "temperature_2m,weather_code",
+        current: "temperature_2m,weather_code,apparent_temperature,relative_humidity_2m,wind_speed_10m,precipitation",
         daily: "temperature_2m_max,temperature_2m_min,weather_code",
         timezone: 'auto'
     });
@@ -17,9 +17,11 @@ async function getWeatherData(latitude, longitude) {
         const data = await response.json();
         renderWeather(data.daily)
         renderCurrent(data.current)
+        renderCurrentInfo(data.current)
     } catch (error) {
         console.error("Fetch error:", error)
     }
 }
-getWeatherData(3.1292, 101.6165);
+getWeatherData(3.1292, 101.6165); // KL
+// getWeatherData(40.0583, 74.4057); // new jersey
 
